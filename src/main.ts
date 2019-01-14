2ccc51e93c8648da9e970213918cd9f5788098b1
Marek Pilczuk <marek@pilczuk.net>
2019-01-14 11:54:18 +0100

    Initialization

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import "dvlib";
+import { 
+    createCanvas, dvStart, resizeCanvas, background, fill, 
+    textAlign, HAlignment, textSize, text, width, height, warmGrayLight, greenLight 
+} from "dvlib";
+
+dvStart(setup, draw);
+
+function setup(): void {
+    let body: HTMLElement = document.getElementsByTagName('body')[0];
+    createCanvas(body);
+    resizeCanvas(600, 300);
+}
+
+function draw() {
+    background('#2d2f2f');
+    fill(greenLight);
+    textAlign(HAlignment.center);
+    textSize(48);
+    text('Hello, World!', width / 2, height / 2);
+}
